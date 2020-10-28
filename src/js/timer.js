0ca420c709c1b6a86e977c5f55433e2e899328e6
Aleksey Gelich <alexeygelich@gmail.com>
2020-10-29 00:54:02 +0200

    up

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,19 +1,19 @@
 class CountdownTimer { 
-    constructor(year,month,day) {
-    this.selector = Date.now();
+    constructor(timerID,year,month,day) {
+    this.selector = timerID;
     this.targetDate = (new Date(year,month,day)).getTime();
     }
 }
-
-const dayRef = document.querySelector('span[data-value="days"]');
-const hoursRef = document.querySelector('span[data-value="hours"]');
-const minsRef = document.querySelector('span[data-value="mins"]');
-const secsRef = document.querySelector('span[data-value="secs"]');
+const date = new CountdownTimer('#timer-1', 2021, 0, 1);
+const timerRef = document.querySelector(date.selector)
+const dayRef = timerRef.querySelector('span[data-value="days"]');
+const hoursRef = timerRef.querySelector('span[data-value="hours"]');
+const minsRef = timerRef.querySelector('span[data-value="mins"]');
+const secsRef = timerRef.querySelector('span[data-value="secs"]');
 
 
 setInterval(()=>{
-    const date = new CountdownTimer(2021, 0, 1);
-    const time = date.targetDate - date.selector;
+    const time = date.targetDate - Date.now();
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
